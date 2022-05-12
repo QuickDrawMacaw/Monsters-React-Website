6742e2a381fe0b1845c0d09e303171068b13b531
Yuki Tabata <ytabata11@gmail.com>
2022-05-12 14:59:36 +1000

    commit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,21 @@
 import { Component } from 'react'
-import './card-list.styles.css';
-
+import '../card-list/card-list.styles.css';
+import Card from '../card/card.component';
 class CardList extends Component {
     
     render() {
-        console.log('Render');
+        
         const { monsters } = this.props;
         
         return(
             <div className='card-list'>
                 {monsters.map((monster) => {
-                    const { name, email, id } = monster; 
                     return(
-                    <div className='card-container' key={id}> 
-                        <img alt={`monster ${name}`}
-                        src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
+                        <Card 
+                          key={monster.id}
+                          monster={monster}
+                         
+                        />      
                 )}) 
                 }
             </div>
