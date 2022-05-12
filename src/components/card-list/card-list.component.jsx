ed651b9a90ae3da8262b9fddd31c42a8dec6d28d
Yuki Tabata <ytabata11@gmail.com>
2022-05-12 14:24:35 +1000

    commit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,13 +5,20 @@ class CardList extends Component {
     render() {
         console.log('Render');
         const { monsters } = this.props;
+        
         return(
-            <div >
-                {monsters.map((monster) => (
-                    <div key={monster.id}>
-                        <h1>{monster.name}</h1>
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster; 
+                    return(
+                    <div className='card-container' key={id}> 
+                        <img alt={`monster ${name}`}
+                        src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
                     </div>
-                )) }
+                )}) 
+                }
             </div>
         ) 
         
